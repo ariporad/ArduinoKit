de1bcc9c0ff8a15249d78d3de401b7abb44e4ba3
Ari Porad <ariporad@seattleacademy.org>
2014-09-07 09:25:24 -0700

    Added ORSSerialPort Library

--- ArduinoKit/ArduinoKit/Communicator.swift
+++ ArduinoKit/ArduinoKit/Communicator.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+import IOKit
+import sys
+
+// This will take care of connecting to the arduino. It will connect to the first one running ArduinoKit it finds.
+func connect() {
+	var ioObject: io_object_t;
+	var ioIterator: io_iterator_t;
+	
+	IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching(kIOSerialBSDServiceValue), &ioIterator);
+	
+}
